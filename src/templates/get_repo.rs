42919dffaa54ff2701ea84eeccf5230a19e938e7
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-12 19:37:11 +0100

    add method to get from template

--- src/templates/get_repo.rs
+++ src/templates/get_repo.rs
@@ -1,1 +1,16 @@
-use crate::repository
+use crate::repository::repo::*;
+
+impl Repository {
+    pub fn get_from_template(key: &str, name: &str) -> Result<Repository, std::io::Error> {
+        let mut repo = Repository::new();
+        let template_repo = Repository::from(&format!("src/templates/{}", key))?;
+
+        for (key, value) in template_repo.files {
+            let new_key = str::replace(&key, "NAME", name);
+            let new_content = str::replace(&value, "NAME", name);
+            repo.files.insert(new_key, new_content);
+        }
+
+        Ok(repo)
+    }
+}
