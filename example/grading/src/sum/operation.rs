f326d218e6bb949c678ae28a9681f9aa3427a3ad
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-12 20:44:05 +0100

    add comments to differ equal methods

--- example/grading/src/sum/operation.rs
+++ example/grading/src/sum/operation.rs
@@ -1,5 +1,7 @@
 /**
  * Adds two numbers and returns the result.
+ * This is the solution repository and it's expected
+ * for these methods to work with 100% Test accuracy.
  */
 pub fn add(left: usize, right: usize) -> usize {
     left + right
--- example/to-grade/src/sum/operation.rs
+++ example/to-grade/src/sum/operation.rs
@@ -1,5 +1,7 @@
 /**
  * Adds two numbers and returns the result.
+ * This is the to-grade repository. It's expected
+ * to test the behaviour of this method.
  */
 pub fn add(left: usize, right: usize) -> usize {
     left + right
