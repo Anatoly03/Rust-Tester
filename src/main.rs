bc191321f2d8e1757cff30f8cda7b5b1d655cb1c
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-12 11:37:48 +0100

    add driver example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![feature(rustc_private)]
 
 // mod ast_reader;
-mod diagnostics;
+// mod diagnostics;
+mod driver;
 
 fn main() {
     // ast_reader::run();
-    diagnostics::run();
+    // diagnostics::run();
+    driver::run();
 }
