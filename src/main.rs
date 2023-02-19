0731cd397b4f00f2abd6112710daacca5902fa28
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-19 12:20:36 +0100

    add data retriever from test cases

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 
 use std::process::Command;
 
-mod repository;
 mod arg_reader;
+mod repository;
 
 // mod ast_reader;
 // mod diagnostics;
@@ -11,11 +11,11 @@ mod arg_reader;
 // mod running_code;
 
 /**
-    RUN WITH
-    ```
-    cargo run -- --grade example/to-grade --test example/grading
-    ```
- */
+   RUN WITH
+   ```
+   cargo run -- --grade example/to-grade --test example/grading
+   ```
+*/
 
 fn main() {
     let config = arg_reader::util::get_config();
@@ -30,14 +30,42 @@ fn main() {
     // TODO change to use rustc
 
     let echo = Command::new("cargo")
-            .current_dir("debug_test")
-            .args(["test", "--", "-Z", "unstable-options", "--format", "json"])
-            .output()
-            .expect("failed to execute process");
+        .current_dir("debug_test")
+        .args(["test", "--", "-Z", "unstable-options", "--format", "json"])
+        .output()
+        .expect("failed to execute process");
 
-    let s = String::from_utf8(echo.stdout).unwrap(); //.lines();
+    let output = String::from_utf8(echo.stdout).unwrap();
+    let mut data = output.lines().map(|i| json::parse(i).unwrap());
 
-    println!("{}", s);
+    // println!("{:?}", output.lines());
+
+    let test_count = &data.next().unwrap()["test_count"].as_i32().unwrap();
+
+    println!("{:?}", 1 .. *test_count * 2);
+
+    for _ in 0 .. *test_count * 2 {
+        let value = &data.next().unwrap();
+
+        println!("{value}");
+
+        if value["event"] == "started" {
+            //continue;
+        }
+
+        let status = value["event"] == "ok";
+        let name = value["name"].as_str().unwrap();
+
+        println!("{name}");
+
+        if value["event"] == "failed" {
+            let stdout = value["stdout"].as_str().unwrap();
+        }
+    }
+
+    let summary = &data.next().unwrap();
+
+    // println!("{}", summary);
 
     // println!("{:#?}", generated.files);
 
